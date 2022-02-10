6dcdb8813b9e8b64e00008c367cac68f196a3e82
Trevor-Davis <56545070+Trevor-Davis@users.noreply.github.com>
2022-02-10 15:54:57 -0500

    commit

--- AVS-PC-HCX-Networking-FULL/addhcx.ps1
+++ AVS-PC-HCX-Networking-FULL/addhcx.ps1
@@ -1,5 +1,3 @@
-
-
 write-Host -ForegroundColor Yellow "Deploying VMware HCX to the $pcname Private Cloud ... This will take approximately 45 minutes ... "
 az vmware addon hcx create --resource-group "VirtualWorkloads-APAC-AzureCloud" --private-cloud "AVS1-VirtualWorkloads-APAC-AzureCloud" --offer "VMware MaaS Cloud Provider"
 write-Host -ForegroundColor Green "Success: VMware HCX has been deployed to $pcname Private Cloud"
