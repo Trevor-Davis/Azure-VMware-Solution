9cb72d8c3e8aa529d70cc8e1bce37bc85f18e33b
Trevor-Davis <56545070+Trevor-Davis@users.noreply.github.com>
2022-02-10 15:59:08 -0500

    afdssdf

--- AVS-PC-HCX-Networking-FULL/avspcdeploy.ps1
+++ AVS-PC-HCX-Networking-FULL/avspcdeploy.ps1
@@ -59,30 +59,28 @@ if ($PSVersionTable.PSVersion.Major -lt 7){
 $filename = "ConnectToAzure.ps1"
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/Trevor-Davis/Azure-VMware-Solution/master/AVS-PC-HCX-Networking-FULL/$filename -OutFile $env:TEMP\AVSDeploy\$filename
 . "$env:TEMP\AVSDeploy\$filename"
-<#
+
 $filename = validatesubready.ps1
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/Trevor-Davis/Azure-VMware-Solution/master/AVS-PC-HCX-Networking-FULL/$filename -OutFile $env:TEMP\AVSDeploy\$filename
-Invoke-Expression $env:TEMP\AVSDeploy\$filename
+. "$env:TEMP\AVSDeploy\$filename"
 
 $filename = DefineResourceGroup.ps1
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/Trevor-Davis/Azure-VMware-Solution/master/AVS-PC-HCX-Networking-FULL/$filename -OutFile $env:TEMP\AVSDeploy\$filename
-Invoke-Expression $env:TEMP\AVSDeploy\$filename
+. "$env:TEMP\AVSDeploy\$filename"
 
 $filename = kickoffdeploymentofavsprivatecloud.ps1
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/Trevor-Davis/Azure-VMware-Solution/master/AVS-PC-HCX-Networking-FULL/$filename -OutFile $env:TEMP\AVSDeploy\$filename
-Invoke-Expression $env:TEMP\AVSDeploy\$filename
+. "$env:TEMP\AVSDeploy\$filename"
 
 $filename = ConnectAVSExrToVnet.ps1
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/Trevor-Davis/Azure-VMware-Solution/master/AVS-PC-HCX-Networking-FULL/$filename -OutFile $env:TEMP\AVSDeploy\$filename
-Invoke-Expression $env:TEMP\AVSDeploy\$filename
-#>
+. "$env:TEMP\AVSDeploy\$filename"
+
 $filename = "ConnectAVSExrToOnPremExr.ps1"
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/Trevor-Davis/Azure-VMware-Solution/master/AVS-PC-HCX-Networking-FULL/$filename -OutFile $env:TEMP\AVSDeploy\$filename
 . "$env:TEMP\AVSDeploy\$filename"
 
-<#
 $filename = "addhcx.ps1"
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/Trevor-Davis/Azure-VMware-Solution/master/AVS-PC-HCX-Networking-FULL/$filename -OutFile $env:TEMP\AVSDeploy\$filename
 . "$env:TEMP\AVSDeploy\$filename"
-#>
 
