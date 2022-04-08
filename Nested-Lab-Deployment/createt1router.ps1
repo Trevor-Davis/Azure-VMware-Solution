94b40ab7ac59c5f7de8876c6757bae2f4c1137d7
Trevor-Davis <56545070+Trevor-Davis@users.noreply.github.com>
2022-04-08 20:08:03 +0000

    update

---
+++ Nested-Lab-Deployment/createt1router.ps1
@@ -0,0 +1,23 @@
+#########################################
+# Get Tier-0 Routers
+#########################################
+
+logintonsx
+
+$body = "{
+`n    `"router_type`": `"TIER1`",
+`n    `"resource_type`": `"LogicalRouter`",
+`n                `"tags`": [
+`n                {
+`n                    `"scope`": `"T1Router`",
+`n                    `"tag`": `"$NestedBuildName`"
+`n                }
+`n            ],
+`n        `"high_availability_mode`": `"ACTIVE_STANDBY`",
+`n    `"failover_mode`": `"NON_PREEMPTIVE`",
+`n   `"display_name`": `"$NestedBuildName-T1Router`",
+`n    `"_revision`": 0
+`n}"
+
+$response = Invoke-RestMethod https://$global:nsxtip/api/v1/logical-routers/ -Method 'POST' -Headers $headers -Body $body -SkipCertificateCheck
+$response | ConvertTo-Json
