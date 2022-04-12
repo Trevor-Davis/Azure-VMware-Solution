b880108a066a6eeffeca9e2ad3224ee4014a7e8b
Trevor-Davis <56545070+Trevor-Davis@users.noreply.github.com>
2022-04-12 17:06:40 -0400

    updtaes

---
+++ Nested-Lab-Deployment/delete.ps1
@@ -0,0 +1,46 @@
+logintonsx
+
+$response = Invoke-RestMethod `
+https://$nsxtip/api/v1/logical-router-ports/$global:wandownlinkrouterportid -Method 'DELETE' -Headers $headers -SkipCertificateCheck
+$response | ConvertTo-Json
+
+$response = Invoke-RestMethod `
+https://$nsxtip/api/v1/logical-router-ports/$global:mgmtdownlinkrouterportid -Method 'DELETE' -Headers $headers -SkipCertificateCheck
+$response | ConvertTo-Json
+
+$response = Invoke-RestMethod `
+https://$nsxtip/api/v1/logical-routers/$global:NestedLabT1RouterID -Method 'DELETE' -Headers $headers -SkipCertificateCheck
+$response | ConvertTo-Json
+
+$response = Invoke-RestMethod `
+https://$nsxtip/api/v1/logical-ports/$global:mgmtuplinkswitchportid -Method 'DELETE' -Headers $headers -SkipCertificateCheck
+$response | ConvertTo-Json
+
+$response = Invoke-RestMethod `
+https://$nsxtip/api/v1/logical-ports/$global:wanuplinkswitchportid -Method 'DELETE' -Headers $headers -SkipCertificateCheck
+$response | ConvertTo-Json
+
+$response = Invoke-RestMethod `
+https://$nsxtip/api/v1/logical-switches/$global:mgmtswitchid -Method 'DELETE' -Headers $headers -SkipCertificateCheck
+$response | ConvertTo-Json
+
+$response = Invoke-RestMethod `
+https://$nsxtip/api/v1/logical-switches/$global:wanswitchid -Method 'DELETE' -Headers $headers -SkipCertificateCheck
+$response | ConvertTo-Json
+
+$response = Invoke-RestMethod `
+https://$nsxtip/api/v1/logical-switches/$NestedBuildName-Workloads-VLAN-$vlanid -Method 'DELETE' -Headers $headers -SkipCertificateCheck
+$response | ConvertTo-Json
+
+$response = Invoke-RestMethod `
+https://$nsxtip/api/v1/switching-profiles/$NestedBuildName-IPDiscovery -Method 'DELETE' -Headers $headers -SkipCertificateCheck
+$response | ConvertTo-Json
+
+$response = Invoke-RestMethod `
+https://$nsxtip/api/v1/switching-profiles/$NestedBuildName-MACManagement -Method 'DELETE' -Headers $headers -SkipCertificateCheck
+$response | ConvertTo-Json
+
+$response = Invoke-RestMethod `
+https://$nsxtip/api/v1/switching-profiles/$NestedBuildName-SwitchSecurity -Method 'DELETE' -Headers $headers -SkipCertificateCheck
+$response | ConvertTo-Json
+
--- Nested-Lab-Deployment/linkswitchandt1.ps1
+++ Nested-Lab-Deployment/linkswitchandt1.ps1
@@ -13,7 +13,7 @@ $body = "{
     `n        }
     `n    ],
     `n    `"linked_logical_switch_port_id`": {
-    `n        `"target_id`": `"$global:mgmtswitchid`",
+    `n        `"target_id`": `"$global:mgmtuplinkswitchportid`",
     `n        `"target_display_name`": `"Switch Port`",
     `n        `"target_type`": `"LogicalPort`",
     `n        `"is_valid`": true
@@ -31,5 +31,40 @@ $body = "{
     `n    `"_revision`": 0
     `n}"
     
-    $response = Invoke-RestMethod https://$nsxtip/api/v1/logical-router-ports/1b9d3c94-391c-4265-a461-a0d370dedcb8 -Method 'PUT' -Headers $headers -Body $body -SkipCertificateCheck
+    $response = Invoke-RestMethod https://$nsxtip/api/v1/logical-router-ports/$global:mgmtdownlinkrouterportid -Method 'PUT' -Headers $headers -Body $body -SkipCertificateCheck
+    $response | ConvertTo-Json
+
+
+###################################
+#WAN Link
+###################################
+$body = "{
+    `n    `"subnets`": [
+    `n        {
+    `n            `"ip_addresses`": [
+    `n                `"$global:wannetworkgateway`"
+    `n            ],
+    `n            `"prefix_length`": 24
+    `n        }
+    `n    ],
+    `n    `"linked_logical_switch_port_id`": {
+    `n        `"target_id`": `"$global:wanuplinkswitchportid`",
+    `n        `"target_display_name`": `"Switch Port`",
+    `n        `"target_type`": `"LogicalPort`",
+    `n        `"is_valid`": true
+    `n    },
+    `n    `"urpf_mode`": `"STRICT`",
+    `n    `"resource_type`": `"LogicalRouterDownLinkPort`",
+    `n    `"id`": `"$global:wandownlinkrouterportid`",
+    `n    `"tags`": [
+    `n        {
+    `n            `"scope`": `"t1downlinkport`",
+    `n            `"tag`": `"$NestedBuildName`"
+    `n        }
+    `n    ],
+    `n    `"logical_router_id`": `"$NestedLabT1RouterID`",
+    `n    `"_revision`": 0
+    `n}"
+    
+    $response = Invoke-RestMethod https://$nsxtip/api/v1/logical-router-ports/$global:wandownlinkrouterportid -Method 'PUT' -Headers $headers -Body $body -SkipCertificateCheck
     $response | ConvertTo-Json
