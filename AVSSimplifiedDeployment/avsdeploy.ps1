05cd38ba60102725cfac17dbe43703ba4bfe28d3
Trevor-Davis <56545070+Trevor-Davis@users.noreply.github.com>
2022-07-13 11:12:18 -0400

    updates

--- AVSSimplifiedDeployment/avsdeploy.ps1
+++ AVSSimplifiedDeployment/avsdeploy.ps1
@@ -95,43 +95,6 @@ Invoke-WebRequest -uri "https://raw.githubusercontent.com/Trevor-Davis/Azure-VMw
 Clear-Host
 Invoke-Expression -Command $env:TEMP\AVSDeploy\$filename
 
-azurelogin -subtoconnect $sub
-
-$testforpc = get-azvmwareprivatecloud -Name $pcname -ResourceGroupName $rgfordeployment -ErrorAction SilentlyContinue
-if ($testforpc.count -eq 1) {
-  $pcdeployed=1
-}
-
-if ($pcdeployed -eq 0){
-Write-Host -ForegroundColor Yellow  "
-Validating Subscription Readiness ..." 
-$quota = Test-AzVMWareLocationQuotaAvailability -Location $regionfordeployment -SubscriptionId $sub -ErrorAction Stop
-if ("Enabled" -eq $quota.Enabled)
-{
-
-Write-Host -ForegroundColor Green "
-Success: Quota is Enabled on Subscription"    
-
-}
-
-Else
-
-{
-Write-Host -ForegroundColor Red "
-Subscription $sub Does NOT Have Quota for Azure VMware Solution, please visit the following site for guidance on how to get this service enabled for your subscription."
-
-Write-Host -ForegroundColor White "
-https://docs.microsoft.com/en-us/azure/azure-vmware/enable-azure-vmware-solution
-"
-
-Set-ItemProperty -Path "HKCU:\Console" -Name Quickedit $quickeditsettingatstartofscript.QuickEdit
-
-Exit
-
-}
-
-}
-
 #######################################################################################
 # Define The Resource Group For AVS Deploy
 #######################################################################################
