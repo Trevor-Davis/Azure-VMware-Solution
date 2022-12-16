efe5d546a6ca3ce71e0383595ea8b3c13cf9e1b2
tredavis <trevor.davis@hotmail.com>
2022-12-16 13:37:24 -0500

    updates

--- AVS-Deployment/ConnectToAzure.ps1
+++ AVS-Deployment/ConnectToAzure.ps1
@@ -1,5 +1,19 @@
-########## Connect To Azure  #######################################
-write-host -ForegroundColor Green "
+    $sublist = @()
+    $sublist = Get-AzSubscription -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
+    $checksub = $sublist -match $sub
+    $getazcontext = Get-AzContext
+    
+If ($checksub.Count -eq 1 -and $getazcontext.Subscription.Id -eq $sub) {
+Write-host -foregroundcolor Blue "
+$sub Already Connected ... Skipping to Next Step"}
 
+if ($checksub.Count -eq 1 -and $getazcontext.Subscription.Id -ne $sub) {
+Set-AzContext -SubscriptionId $sub -WarningAction SilentlyContinue
+write-host -foregroundcolor Blue "
+$sub Already Connected ... Skipping to Next Step"}
+
+if ($checksub.Count -eq 0) {
+write-host -ForegroundColor Green "
 Connecting to your Azure Subscription $sub ... there should be a web browser pop-up ... go there to login"
-Connect-AzAccount -Subscription $sub 
+Connect-AzAccount -Subscription $sub -WarningAction SilentlyContinue
+}
--- AVS-Deployment/registeravsresourceprovider.ps1
+++ AVS-Deployment/registeravsresourceprovider.ps1
@@ -1,11 +1,24 @@
 
-$status = Get-AzResourceProvider -ProviderNamespace Microsoft.AVS -Location $regionfordeployment -ErrorAction SilentlyContinue
+$test = Get-AzResourceProvider -ProviderNamespace Microsoft.AVS -Location $regionfordeployment -ErrorAction SilentlyContinue
 
-if ($status.RegistrationState -eq "NotRegistered") {
-  Register-AzResourceProvider -ProviderNamespace Microsoft.AVS
+if ($test.RegistrationState -eq "Registered") {
+write-Host -ForegroundColor Blue "
+Microsoft.AVS Resource Provider Is Already Registered ... Skipping to Next Step"
+  }
+  
+if ($test.RegistrationState -eq "NotRegistered") {
+write-host -foregroundcolor Yellow "
+Registering Microsoft.AVS Resource Provider"
+Register-AzResourceProvider -ProviderNamespace Microsoft.AVS
+
+$test = Get-AzResourceProvider -ProviderNamespace Microsoft.AVS -Location $regionfordeployment -ErrorAction SilentlyContinue
+If(test.RegistrationState -eq "NotRegistered"){
+Write-Host -ForegroundColor Red "
+Microsoft.AVS Resource Provider Registration Failed"
+Exit
 }
-
-if ($status.RegistrationState -eq "Registered") {
-  write-Host -ForegroundColor Blue "
-Microsoft.AVS Resource Provider Is Already Registered"
+else {
+  write-Host -ForegroundColor Green "
+  Microsoft.AVS Resource Provider Successfully Registered ... Skipping to Next Step"
+  }
 }
