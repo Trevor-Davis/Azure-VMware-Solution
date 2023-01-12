613b76b25f1e2a9d959999cf7ccd6e6a6d9a5466
tredavis <trevor.davis@hotmail.com>
2023-01-12 12:09:59 -0500

    jklkjlkj

--- AVS-Deployment/01_deployazurevmwaresolution.ps1
+++ AVS-Deployment/01_deployazurevmwaresolution.ps1
@@ -37,7 +37,7 @@ $filename = "deployavsprivatecloud.ps1"
 write-host "Downloading" $filename
 Invoke-WebRequest -uri "https://raw.githubusercontent.com/Trevor-Davis/Azure-VMware-Solution/master/AVS-Deployment/$filename" -OutFile $env:TEMP\$folder\$filename
 . $env:TEMP\$folder\$filename 
-
+<#
 ##################################
 #Create ExpressRoute Gateway (Function)
 ##################################
@@ -67,95 +67,4 @@ write-host "Downloading" $filename
 Invoke-WebRequest -uri "https://raw.githubusercontent.com/Trevor-Davis/Azure-VMware-Solution/master/AVS-Deployment/$filename" -OutFile $env:TEMP\$folder\$filename
 . $env:TEMP\$folder\$filename 
 
-<#
-#Variables
-$global:regionfordeployment = "West US"
-$global:pcname = "Prod_Private_Cloud"
-$global:avssku = "AV36"
-$global:addressblock = "192.168.4.0/22"
-$global:ExrGatewayForAVS = "THIS WILL NEED TO BEPOPULATED BY SCRIPT" ##this only gets filled in for ExpressRoute connected on-prem sites.
-$global:VWanHUBNameWithExRGW = "VirtualWorkloads-vWANHub" ##NEW
-$global:deployhcxyesorno = "No"
-$global:ExrGWforAVSResourceGroup = "VirtualWorkloads"
-$global:NameOfOnPremExRCircuit = "prod_express_route" 
-$global:ExRGWForAVSRegion = "westus"
-$global:AzureConnection = "ExpressRoute"
-
-$global:RGofOnPremExRCircuit = "Prod_AVS_RG"  
-$global:internet = "Enabled"
-$global:numberofhosts = "3"
-
-$global:RGNewOrExisting = "New" #RGforAVSNewOrExisting
-if("New" -eq $global:RGNewOrExisting)
-{
-$global:rgfordeployment = "Prod_RG"
-}
-else {
-$global:rgfordeployment = "" #rgfordeployment
-}
-
-
-$global:SameSubAVSAndExRGW = "Yes"
-if ("Yes" -eq $global:SameSubAVSAndExRGW) {
-$global:vnetgwsub = $sub
-}
-else {
-$global:vnetgwsub = ""
-}
-
-$global:OnPremExRCircuitSub = ""
-$global:OnPremVIServerIP = ""
-$global:PSCSameAsvCenterYesOrNo = ""
-if ($global:PSCSameAsvCenterYesOrNo -eq "Yes" ) {
-  $global:PSCIP = $global:OnPremVIServerIP
-}
-else {
-  $global:PSCIP = ""
-}
-$global:HCXOnPremRoleMapping = ""
-
-$global:VpnGwVnetName = "" #name of the vNet where the current VPN GW Exists.
-
-$global:RouteServerSubnetAddressPrefix = ""
-
-$global:OnPremCluster = ""
-
-$global:vmotionportgroup =  ""
-$global:vmotionprofilegateway = ""
-$global:vmotionnetworkmask = ""
-$global:vmotionippool = ""
-
-$global:managementportgroup = ""
-$global:mgmtprofilegateway = ""
-$global:mgmtnetworkmask = ""
-$global:mgmtippool = ""
-
-$global:VMNetwork = ""
-$global:Datastore = ""
-
-$global:HCXManagerVMName = ""
-$global:HCXVMIP = ""
-$global:HCXVMNetmask = ""
-$global:HCXVMGateway = ""
-$global:HCXVMDNS = ""
-$global:HCXVMDomain = ""
-$global:AVSVMNTP = ""
-$global:HCXOnPremLocation = ""
-$global:hcxVDS = ""
-$global:l2networkextension = ""
-     
-$global:HCXOnPremUserID = "admin"
-     $global:mgmtnetworkprofilename = "Management"
-     $global:vmotionnetworkprofilename = "vMotion"
-     $global:hcxactivationurl = "https://connect.hcx.vmware.com"
-     $global:HCXCloudUserID = "cloudadmin@vsphere.local"
-     $global:hcxComputeProfileName = "AVS-ComputeProfile"
-     $global:hcxServiceMeshName = "AVS-ServiceMesh"
-$global:hcxfilename = "VMware-HCX-Connector-4.3.1.0-19373134.ova"
-
-
-##Anything being done here is because the variable name needs to exist for other scripts being pulled in.
-$global:resourcegroupname = $global:rgfordeployment
-$global:region = $global:regionfordeployment
-$global:VIServerIP = $global:OnPremVIServerIP
 #>
