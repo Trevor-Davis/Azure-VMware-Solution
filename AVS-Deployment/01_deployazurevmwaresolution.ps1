d856879d18df6e192a928d5b986b3d4ff0843b10
tredavis <trevor.davis@hotmail.com>
2023-01-14 12:39:30 -0500

    updates

--- AVS-Deployment/01_deployazurevmwaresolution.ps1
+++ AVS-Deployment/01_deployazurevmwaresolution.ps1
@@ -37,7 +37,7 @@ $filename = "deployavsprivatecloud.ps1"
 write-host "Downloading" $filename
 Invoke-WebRequest -uri "https://raw.githubusercontent.com/Trevor-Davis/Azure-VMware-Solution/master/AVS-Deployment/$filename" -OutFile $env:TEMP\$folder\$filename
 . $env:TEMP\$folder\$filename 
-<#
+
 ##################################
 #Create ExpressRoute Gateway (Function)
 ##################################
@@ -50,7 +50,7 @@ ExpressRoute Gateway $exrgwname Already Exists"
 if ($test.count -eq 0){
 createexrgateway -vnet $exrgwvnet -resourcegroup $exrgwrg -region $exrgwregion -exrgwname $exrgwname
 }
-
+<#
 ##################################
 #Connect AVS to ExR GW
 ##################################
