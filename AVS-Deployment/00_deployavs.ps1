e21dc85c2f6eea733417ea48f4e70e6ec7bf659f
tredavis <trevor.davis@hotmail.com>
2023-01-17 17:15:26 -0500

    updates

--- AVS-Deployment/00_deployavs.ps1
+++ AVS-Deployment/00_deployavs.ps1
@@ -1,9 +1,13 @@
+#################################
 #Variables
+#################################
+
 $global:avssub = "1178f22f-6ce4-45e3-bd92-ba89930be5be" #Sub Where to Deploy AVS
+$global:avsdeploymentfolder = "AVS-Deployment"
 $global:regionfordeployment = "Australia East" #The region where AVS should be deployed
 $global:avsrgname = "VirtualWorkloads-AVS-PC01" #The REsource Group To Deploy AVS, Can be New or Existing
 $global:pcname = "VirtualWorkloads-AVS-PC01" #The name of the AVS Private Cloud
-$global:avsaddressblock = "10.0.4.0/22" #The /22 Network Block for AVS Infra
+$global:avsaddressblock = "10.0.0.0/22" #The /22 Network Block for AVS Infra
 $global:avssku = "AV36" #The AVS SKU Type to Deploy
 $global:numberofhosts = "3" #This should be left at 3
 $global:internet = "Enabled" 
@@ -12,11 +16,11 @@ $global:networkForApplianceVM = "ARCforAVS-Segment" #this is NSX segment name wh
 
 #Azure Backbone Network Connectivity
 $global:exrgwneworexisting = "New" # Set to 'New' if you are creating a new ExpressRoute Gateway for AVS, Set to 'Existing' if an existing ExpressRoute Gateway will be used.
-$global:exrvnetname = "VirtualWorkloads-AVS-vNet" #The vNet where either the ExpressRoute Gateway exists or the vnet where the expressroute gateway will be created.
 
 #Only Use these variables if you are using an existing ExpressRoute Gateway to connect AVS
 If($exrgwneworexisting -eq "Existing"){
 
+  $global:exrvnetname = "" #The name of the vNet where the expressroute gateway already exists.
   $global:exrgwname = "" #The name of the ExpressRoute Gateway
   $global:exrgwrg = "" #The resource group of the ExpressRoute Gateway
   $global:exrgwregion = "" #the region where the ExpressRoute Gateway is located
@@ -25,6 +29,7 @@ If($exrgwneworexisting -eq "Existing"){
 #Only Use these variables if there is a need to create a new expressroute gateway to connect AVS 
 If($exrgwneworexisting -eq "New"){
 
+  $global:exrvnetname = "VirtualWorkloads-AVS-vNet" #The name of the vNet where the expressroute gateway will be created.
   $global:exrgwname = "For-$pcname" #The name of the ExpressRoute Gateway, only modify if you don't want this to be the name.
   $global:exrgwrg = "VirtualWorkloads-AVS-Networking" #The resource group where the ExR Gateway should be deployed
   $global:exrgwregion = "Australia East" #the region where the ExpressRoute Gateway should be deployed.
@@ -43,38 +48,36 @@ $global:rgofonpremexrcircuit = "VirtualWorkloads-AVSPrivateCloud-RG"
 
 #DO NOT MODIFY BELOW THIS LINE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
+#Testing
+$testing = 0
+
+#Functions to Load
+
+$functions = @(
+'CreateDirectoryAndLog.ps1'
+)
+
+foreach ($function in $functions) {
+Invoke-WebRequest -uri "https://raw.githubusercontent.com/Trevor-Davis/scripts/main/Functions/$function" -OutFile $env:TEMP\$function 
+. $env:TEMP\$function
+
+}
+
+
+#Misc
+
 $quickeditsettingatstartofscript = Get-ItemProperty -Path "HKCU:\Console" -Name Quickedit
 Set-ItemProperty -Path "HKCU:\Console" -Name Quickedit 0
 $quickeditsettingatstartofscript.QuickEdit
 Set-Item Env:\SuppressAzurePowerShellBreakingChangeWarnings "true"
 $ProgressPreference = 'SilentlyContinue'
 
-#Testing
-$testing = 0
-
-#Constants
+#Variables Do Not Modify
 $global:nameofavsglobalreachconnection = "to-$nameofonpremexrcircuit"
 $global:avsexrauthkeyname = "to-ExpressRouteGateway-$exrgwname"
 $global:avsexrgwconnectionname = "from-AVSPrivateCloud-$pcname"
 $global:gatewaysubnetname = "GatewaySubnet" #DO NOT MODIFY
 $global:exrgwipname = "$exrgwname-PIP" #DO NOT MODIFY
 
-#Create Directory
-$global:folder = "AVS-Deployment" #This is where all the files will be downloaded to which will be used to deploy AVS
-if (Test-Path -Path $env:TEMP\$folder ) {
-  Remove-Item $env:TEMP\$folder\*.* -Recurse -Force -ErrorAction:Ignore
-} else {
-  mkdir $env:TEMP\$folder 
-}
-
-#Start Logging
-Start-Transcript -Path $env:TEMP\$folder\avsdeploy.log -Append
-
-
-#Begin
-$filename = "01_deployazurevmwaresolution.ps1"
-write-host "Downloading" $filename
-if ($testing -eq 0) {
-Invoke-WebRequest -uri "https://raw.githubusercontent.com/Trevor-Davis/Azure-VMware-Solution/master/AVS-Deployment/$filename" -OutFile $env:TEMP\$folder\$filename
-. $env:TEMP\$folder\$filename
-}
+#Execution
+createdirectoryandlog -folder $env:TEMP -foldername $global:avsdeploymentfolder
