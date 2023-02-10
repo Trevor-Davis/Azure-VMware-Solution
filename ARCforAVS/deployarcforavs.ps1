603f9a6090ab1d719f76df04a9061beb96c002bb
tredavis <trevor.davis@hotmail.com>
2023-02-10 10:07:19 -0500

    Update

--- ARCforAVS/deployarcforavs.ps1
+++ ARCforAVS/deployarcforavs.ps1
@@ -1,30 +1,36 @@
 #variables
-$avssub = $global:avssub
-$folder = $global:folder
-$networkForApplianceVM = $global:networkForApplianceVM #this is NSX segment name which will be created for ARC
-$networkCIDRForApplianceVM = $global:networkCIDRForApplianceVM #input the network gateway in this format, this is the network which will be created 10.1.1.1/24
-$arcfoldername = "ArcOnAVS-2.0.14"
+$avssub = "" #Subscription where AVS private cloud is deployed.
+$avsrg = "" #Resource group where AVS private cloud is deployed.
+$pcname = "" #Name of the AVS Private Cloud
+$networkCIDRForApplianceVM = "" #/28 network for the ARC Appliance 
+$tenant = "" #Optional
 
+#DO NOT MODIFY BELOW THIS LINE #################################################
 
-#Do Not Modify Below This Line
+#Functions To Load
 
-Set-ExecutionPolicy -Scope Process -ExecutionPolicy ByPass
+$filename = "Function-CheckForFileAndDelete.ps1"
+Invoke-WebRequest -uri "https://raw.githubusercontent.com/Trevor-Davis/Scripts/main/Functions/$filename" -OutFile $env:TEMP\$filename
+. $env:TEMP\$filename
 
-#Functions to Load
+#Azure Login
+
 $filename = "Function-azurelogin.ps1"
-write-host "Downloading" $filename
-Invoke-WebRequest -uri "https://raw.githubusercontent.com/Trevor-Davis/AzureScripts/main/Functions/$filename" -OutFile $env:TEMP\$folder\$filename
-. $env:TEMP\$folder\$filename
+Invoke-WebRequest -uri "https://raw.githubusercontent.com/Trevor-Davis/AzureScripts/main/Functions/$filename" -OutFile $env:TEMP\$filename
+. $env:TEMP\$filename
 
-$filename = "checkforfileanddelete.ps1"
-write-host "Downloading" $filename
-Invoke-WebRequest -uri "https://raw.githubusercontent.com/Trevor-Davis/scripts/main/Functions/$filename" -OutFile $env:TEMP\$folder\$filename
-. $env:TEMP\$folder\$filename
+if ($tenanttoconnect -ne "") {
+  azurelogin -subtoconnect $sub -tenanttoconnect $tenant
+}
+else {
+  azurelogin -subtoconnect $sub 
+}
 
+#Execution
 
+azurelogin -subtoconnect $sub
 
-#########################################################################################
-azurelogin -subtoconnect $avssub
+Write-Host -ForegroundColor Yellow "Registering ARC for AVS Resource Providers"
 
 Register-AzResourceProvider -ProviderNamespace Microsoft.ConnectedVMwarevSphere 
 Register-AzResourceProvider -ProviderNamespace Microsoft.ExtendedLocation
@@ -32,24 +38,42 @@ Register-AzResourceProvider -ProviderNamespace Microsoft.KubernetesConfiguration
 Register-AzResourceProvider -ProviderNamespace Microsoft.ResourceConnector 
 Register-AzResourceProvider -ProviderNamespace Microsoft.AVS
 Register-AzProviderPreviewFeature -Name AzureArcForAVS -ProviderNamespace Microsoft.AVS
+Register-AzProviderPreviewFeature -Name earlyAccess -ProviderNamespace Microsoft.AVS
+
 
 #Download Zip
-$filename = "ArcOnAVS.zip"
+$filename = "v2.0.14.zip"
 write-host "Downloading" $filename
-Invoke-WebRequest -uri "https://github.com/Trevor-Davis/Azure-VMware-Solution/blob/master/ARCforAVS/$filename" -OutFile $env:TEMP\$folder\$filename
-Expand-Archive -Path $env:TEMP\$folder\$filename -DestinationPath $env:TEMP\$folder\ -Confirm:$false
+Invoke-WebRequest -uri "https://github.com/Azure/ArcOnAVS/archive/refs/tags/$filename" -OutFile $env:TEMP\$filename
+mkdir $env:TEMP\"ARCForAVS" -ErrorAction:Ignore
+Expand-Archive -Path $env:TEMP\$filename -DestinationPath $env:TEMP\"ARCForAVS" -Force
 
-checkfileanddelete -filetodelete $env:TEMP\$folder\$arcfoldername\src\config_avs.json
+# Create JSON
 
-$payload = @{
-    "subscriptionId" = $global:avssub
-    "resourceGroup" = $global:avsrgname
-    "privateCloud" = $global:pcname
-    "isStatic" = $true
-  }
-$data = @{"networkForApplianceVM" = $networkforappliancevm;"networkCIDRForApplianceVM" = $networkCIDRForApplianceVM;}
+Out-File -FilePath $env:TEMP\ARCForAVS\ArcOnAVS-2.0.14\src\config_avs.json -Encoding utf8
+#Out-File -FilePath $env:TEMP\ARCForAVS\config_avs.json -Encoding:utf8
+$myjsonfile = $env:TEMP+"\ARCForAVS\ArcOnAVS-2.0.14\src\config_avs.json"
 
-$payload.Add("staticIpNetworkDetails",$data)
-$payload | ConvertTo-Json | Out-File $env:TEMP\$folder\$arcfoldername\src\config_avs.json
+#$jason = $filelinearray | ConvertTo-Json
 
-.$env:TEMP\$folder\$arcfoldername\src\run.ps1 -Operation onboard -FilePath $env:TEMP\$folder\$arcfoldername\src\config_avs.json
+Add-Content -Value ("{") -Path $myjsonfile -Encoding:utf8 
+Add-Content -Value ('"subscriptionId"'+":"+" "+'"'+$avssub+'"'+",") -Path $myjsonfile -Encoding:utf8 
+Add-Content -Value ('"resourceGroup"'+":"+" "+'"'+$avsrg+'"'+",")  -Path $myjsonfile -Encoding:utf8
+#Add-Content -Value ('"applianceControlPlaneIpAddress"'+":"+" "+'"192.168.88.2"'+",")  -Path $myjsonfile -Encoding:utf8
+Add-Content -Value ('"privateCloud"'+":"+" "+'"'+$pcname+'"'+",") -Path $myjsonfile -Encoding:utf8 
+Add-Content -Value ('"isStatic": true,') -Path $myjsonfile -Encoding:utf8 
+Add-Content -Value ('"staticIpNetworkDetails": {') -Path $myjsonfile -Encoding:utf8 
+Add-Content -Value ('"networkForApplianceVM"'+":"+" "+'"ARCForAVSSegment"'+",") -Path $myjsonfile -Encoding:utf8 
+#Add-Content -Value ('"networkCIDRForApplianceVM"'+":"+" "+'"'+$networkCIDRForApplianceVM+'"'+",") -Path $myjsonfile -Encoding:utf8 
+Add-Content -Value ('"networkCIDRForApplianceVM"'+":"+" "+'"'+$networkCIDRForApplianceVM+'"') -Path $myjsonfile -Encoding:utf8 
+#Add-Content -Value ('"k8sNodeIPPoolStart"'+":"+" "+'"192.168.88.3"'+",") -Path $myjsonfile -Encoding:utf8 
+#Add-Content -Value ('"k8sNodeIPPoolEnd"'+":"+" "+'"192.168.88.10"'+",") -Path $myjsonfile -Encoding:utf8 
+#Add-Content -Value ('"gatewayIPAddress"'+":"+" "+'"192.168.88.1"') -Path $myjsonfile -Encoding:utf8 
+Add-Content -Value ("}") -Path $myjsonfile -Encoding:utf8 
+Add-Content -Value ("}") -Path $myjsonfile -Encoding:utf8
+
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy ByPass
+Set-Location -Path $env:TEMP\ARCForAVS\ArcOnAVS-2.0.14\src
+.\run.ps1 -Operation onboard -FilePath .\config_avs.json
+
+
