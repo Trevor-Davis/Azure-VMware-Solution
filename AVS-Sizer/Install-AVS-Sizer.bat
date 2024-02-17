033c38dfe67746dbb6f1ced85029d219e6e264e8
tredavis <trevor.davis@hotmail.com>
2024-02-17 17:02:03 -0500

    lkjljljklji

--- AVS-Sizer/Install-AVS-Sizer.bat
+++ AVS-Sizer/Install-AVS-Sizer.bat
@@ -1,3 +1,2 @@
-curl https://raw.githubusercontent.com/Trevor-Davis/Azure-VMware-Solution/master/AVS-Sizer/avsinstall.ps1 --output %userprofile%\Downloads\avsinstall.ps1
-cd \%userprofile%\Downloads\
+curl https://raw.githubusercontent.com/Trevor-Davis/Azure-VMware-Solution/master/AVS-Sizer/avsinstall.ps1 --output avsinstall.ps1
 powershell.exe -ExecutionPolicy Bypass -File .\avsinstall.ps1
