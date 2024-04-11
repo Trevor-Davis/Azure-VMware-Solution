cf2cf24683383ea07fd3d287a9b9d2489b2cea4a
tredavis <trevor.davis@hotmail.com>
2024-04-11 10:19:39 -0400

    Updates

--- AVS-Sizer/globalvariables.ps1
+++ AVS-Sizer/globalvariables.ps1
@@ -1,6 +1,7 @@
 
 $global:apiurl = "https://vmc.vmware.com/api/vmc-sizer/v5/recommendation?vmPlacement=false"
 $global:apiurlstaging = "https://stg.skyscraper.vmware.com/api/vmc-sizer/v5/recommendation"
+#$global:apiurlstaging = "https://vmc.vmware.com/api/vmc-sizer/v5/recommendation"
 $global:filename = "sizer.xlsm" # this is the filename of the sizer file
 $global:buttonclicked = ""
 $global:downloaddirectory = "$env:HOMEPATH\AppData\Local\avssizer"
